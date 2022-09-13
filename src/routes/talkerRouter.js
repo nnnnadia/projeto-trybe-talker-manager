a1078c3ae80986cb296580abd402b649559f7f23
nnnnadia <tristaonadia@gmail.com>
2022-09-13 20:26:49 -0300

    Requisito 02: feat: rota GET -> /talker/:id.

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -5,8 +5,19 @@ const router = express.Router();
 
 router.get('/', async (_req, res) => {
   try {
-    const talkerFile = await talkerFS.readTalkerFile();
-    res.status(200).json(talkerFile);
+    const talkerList = await talkerFS.readTalkerFile();
+    res.status(200).json(talkerList);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const talkerInfo = await talkerFS.getTalkerById(id);
+    if (talkerInfo) return res.status(200).json(talkerInfo);
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
