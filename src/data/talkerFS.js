93c849c0afd3136f6c56564fe15844301e3e7f33
nnnnadia <tristaonadia@gmail.com>
2022-09-16 19:23:10 -0300

    Requisito 06: feat: módulo de exclusão de um `talker` por id.

--- src/data/talkerFS.js
+++ src/data/talkerFS.js
@@ -15,13 +15,23 @@ const getTalkerById = async (id) => {
   return talkerFile.find((talker) => talker.id === +id);
 };
 
+const removeTalkerById = async (id) => {
+  try {
+    const talkerFile = await readTalkerFile();
+    const newTalkerFile = talkerFile.filter((talker) => talker.id !== id);
+    await fs.writeFile(join(__dirname, '../talker.json'), JSON.stringify(newTalkerFile));
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
 const generateId = (talkerFile) => talkerFile[talkerFile.length - 1].id + 1;
 
 const writeTalkerFile = async (talker) => {
   try {
     const talkerFile = await readTalkerFile();
     const id = !talker.id ? generateId(talkerFile) : talker.id;
-    const talkerWithId = { ...talker, id };
+    const talkerWithId = { ...talker, id: +id };
     talkerFile.push(talkerWithId);
     await fs.writeFile(join(__dirname, '../talker.json'), JSON.stringify(talkerFile));
     return talkerWithId;
@@ -34,4 +44,5 @@ module.exports = {
   readTalkerFile,
   getTalkerById,
   writeTalkerFile,
+  removeTalkerById,
 };
